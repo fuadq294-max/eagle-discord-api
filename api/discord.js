5232a01d680f79a894b500fddb3cedef79fb505e
Eagle hub <fuadq294@gmail.com>
2026-09-07 06:01:35 +0400

    Simplify Discord API handler to return static data

--- api/discord.js
+++ api/discord.js
@@ -1,36 +1,6 @@
-export default async function handler(req, res) {
-  const GUILD_ID = "1507082977295994942";
-  const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
-
-  if (!BOT_TOKEN) {
-    return res.status(500).json({ error: "Missing bot token" });
-  }
-
-  try {
-    const response = await fetch(
-      `https://discord.com/api/v10/guilds/${GUILD_ID}?with_counts=true`,
-      {
-        headers: {
-          Authorization: `Bot ${BOT_TOKEN}`
-        }
-      }
-    );
-
-    if (!response.ok) {
-      return res.status(response.status).json({
-        error: "Discord API error"
-      });
-    }
-
-    const data = await response.json();
-
-    res.status(200).json({
-      members: data.approximate_member_count,
-      online: data.approximate_presence_count
-    });
-  } catch (error) {
-    res.status(500).json({
-      error: "Request failed"
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    test: "OK",
+    members: 2110
+  });
 }
